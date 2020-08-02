b948ddc640a6e7f5a0481a0ffc74669aa02469d7
Luiz Fellype <lfcassago@gmail.com>
2020-08-02 10:58:20 -0300

    Updates

---
+++ precache-manifest.e100ea88f2f26bf53d2a703c6660f52a.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17a52622ed4b40c4f4954f46ca2de91c",
+    "url": "/iColege/index.html"
+  },
+  {
+    "revision": "38b43e8694c2df167a05",
+    "url": "/iColege/static/css/2.9ba5b96e.chunk.css"
+  },
+  {
+    "revision": "c195c128e5575c0857bd",
+    "url": "/iColege/static/css/main.b3ae94c9.chunk.css"
+  },
+  {
+    "revision": "38b43e8694c2df167a05",
+    "url": "/iColege/static/js/2.2448e5e6.chunk.js"
+  },
+  {
+    "revision": "c195c128e5575c0857bd",
+    "url": "/iColege/static/js/main.633f5559.chunk.js"
+  },
+  {
+    "revision": "1a1070568acdeb7b488a",
+    "url": "/iColege/static/js/runtime-main.c3104d45.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/iColege/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "c4584a3be5e75b1595685a1798c50743",
+    "url": "/iColege/static/media/flat-avatar.c4584a3b.png"
+  },
+  {
+    "revision": "567f57385ea3dde2c9aec797d07850d2",
+    "url": "/iColege/static/media/line.567f5738.gif"
+  },
+  {
+    "revision": "177cc92d2e8027712a8c1724abd272cd",
+    "url": "/iColege/static/media/open-sans-v15-latin-300.177cc92d.ttf"
+  },
+  {
+    "revision": "27ef0b062b2e221df16f3bbd97c2dca8",
+    "url": "/iColege/static/media/open-sans-v15-latin-300.27ef0b06.svg"
+  },
+  {
+    "revision": "521d17bc9f3526c690e8ada6eee55bec",
+    "url": "/iColege/static/media/open-sans-v15-latin-300.521d17bc.woff"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/iColege/static/media/open-sans-v15-latin-300.60c86674.woff2"
+  },
+  {
+    "revision": "76b56857ebbae3a5a689f213feb11af0",
+    "url": "/iColege/static/media/open-sans-v15-latin-300.76b56857.eot"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/iColege/static/media/open-sans-v15-latin-700.148a6749.eot"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/iColege/static/media/open-sans-v15-latin-700.2e00b263.svg"
+  },
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/iColege/static/media/open-sans-v15-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/iColege/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/iColege/static/media/open-sans-v15-latin-700.d08c09f2.woff2"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/iColege/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/iColege/static/media/open-sans-v15-latin-regular.9dce7f01.eot"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/iColege/static/media/open-sans-v15-latin-regular.bf2d0783.woff"
+  },
+  {
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/iColege/static/media/open-sans-v15-latin-regular.c045b73d.ttf"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/iColege/static/media/open-sans-v15-latin-regular.cffb686d.woff2"
+  },
+  {
+    "revision": "38d77552b0353684a208177482d5b6ee",
+    "url": "/iColege/static/media/primeicons.38d77552.svg"
+  },
+  {
+    "revision": "473e2a746d3c151d7dcaa626a7c84c60",
+    "url": "/iColege/static/media/primeicons.473e2a74.ttf"
+  },
+  {
+    "revision": "71bb3d79dcf18b45ae845409e7c2ada3",
+    "url": "/iColege/static/media/primeicons.71bb3d79.woff"
+  },
+  {
+    "revision": "b8eccb1059ea5faaf6d8b7d457ccfd09",
+    "url": "/iColege/static/media/primeicons.b8eccb10.eot"
+  }
+]);
